c5e541a9c0e76a804bf723bb2cacf3f6b57a321c
jianghaiping <jianghaiping@teshehui.com>
2016-12-19 19:24:58 +0800

    update

--- myblog/routes/index.js
+++ myblog/routes/index.js
@@ -1,10 +1,10 @@
-moudule.expor = function(app){
-	app.get('/',function(req. res){
+﻿module.exports = function (app) {
+	app.get('/',function(req, res){
 		res.redirect('/posts');
 	});
 	
-	app.use('/signup',require('./signup'));
-	app.use('/signin',require('./signin'));
-	app.use('/signout',require('./signout'));
-	app.use('/posts',require('./posts'));
-}
+	app.use('/signup', require('./signup'));
+	app.use('/signin', require('./signin'));
+	app.use('/signout', require('./signout'));
+	app.use('/posts', require('./posts'));
+};
--- myblog/routes/signin.js
+++ myblog/routes/signin.js
@@ -1,20 +1,19 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 
 var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
-// GET /signin ��¼ҳ
-
-router.get('/', checkNotLogin,function(abc){});
-
+//GET /signin 登录页
 router.get('/', checkNotLogin, function(req, res, next){
 	res.send(req.flash());
 });
 
-//POST  /signin �û���¼
+//POST  /signin 用户登录
 router.post('/', checkNotLogin, function(req, res, next){
 	res.send(req.flash());
 });
 
 module.exports = router;
+
+
 
