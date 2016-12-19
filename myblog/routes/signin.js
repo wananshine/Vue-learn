4196c6af3a292c3b659acc771993f561cf651ec9
jianghaiping <jianghaiping@teshehui.com>
2016-12-19 13:57:04 +0800

    在feat1上提交

--- myblog/routes/signin.js
+++ myblog/routes/signin.js
@@ -4,4 +4,13 @@ var router = express.Router();
 var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 // GET /signin µÇÂ¼Ò³
-router.get('/', checkNotLogin);
+router.get('/', checkNotLogin, function(req, res, next){
+	res.send(req.flash());
+});
+
+//POST  /signin ÓÃ»§µÇÂ¼
+router.post('/', checkNotLogin, function(req, res, next){
+	res.send(req.flash());
+});
+
+module.exports = router;
