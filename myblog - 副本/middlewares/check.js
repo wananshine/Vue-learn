67ddc5b748d9125779662a1e31ad5d5b58d9a346
jianghaiping <jianghaiping@teshehui.com>
2017-02-06 10:32:29 +0800

    update

---
+++ myblog - 副本/middlewares/check.js
@@ -0,0 +1,42 @@
+module.exports = {
+	checkLogin: function checkLogin(req,res,next){
+		if (!req.session.user) {
+			req.flash("error","未登录");
+			return res.redirect("/signin");
+		}
+		next();
+	},
+
+	checkNotLogin: function checkNotLogin(req,res,next){
+		if (req.session.user) {
+			req.flash("error","已经登录");
+			return res.redirect("back"); //返回之前的页面
+		}
+		next();
+	}
+};
+
+
+
+
+
+// module.exports = {
+  // checkLogin: function checkLogin(req, res, next) {
+    // if (!req.session.user) {
+      // req.flash('error', '未登录'); 
+      // return res.redirect('/signin');
+    // }
+    // next();
+  // },
+
+  // checkNotLogin: function checkNotLogin(req, res, next) {
+    // if (req.session.user) {
+      // req.flash('error', '已登录'); 
+      // return res.redirect('back');//返回之前的页面
+    // }
+    // next();
+  // }
+// };
+
+
+
---
+++ myblog - 副本/routes/signout.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+
+//GET /signout 登出
+router.get('/', function(req, res, next){
+	res.send(req.flash());
+});
+
+module.exports = router;
+
+
